200dfe881282ac1e6d1fd15e3a3e7e15887f5e47
ScottAgirs <scott@ijs.to>
2020-08-30 16:08:57 -0400

    [rename package] + [init playground]
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ pages/playground.js
@@ -0,0 +1,23 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>Playground</h1>
+
+      <p>Hello.</p>
+      <style jsx>{`
+        * {
+          line-height: 1.5;
+        }
+        code {
+          padding: 0.2em 0.4em;
+          margin: 0;
+          font-size: 85%;
+          background-color: rgba(27, 31, 35, 0.05);
+          border-radius: 3px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default About;
