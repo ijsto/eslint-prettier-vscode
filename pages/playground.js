ac3fff44a5372a585ebd177ef307ecbd6384be63
ScottAgirs <scott@ijs.to>
2020-08-30 16:31:31 -0400

    nts - move stuff around
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- pages/playground.js
+++ pages/playground.js
@@ -1,7 +1,9 @@
+const pageTitle = 'Playground';
+
 export const About = () => {
   return (
     <div>
-      <h1>Playground</h1>
+      <h1>{pageTitle}</h1>
 
       <p>Hello.</p>
       <style jsx>{`
@@ -9,11 +11,12 @@ export const About = () => {
           line-height: 1.5;
         }
         code {
-          padding: 0.2em 0.4em;
-          margin: 0;
-          font-size: 85%;
           background-color: rgba(27, 31, 35, 0.05);
           border-radius: 3px;
+          color: #b838f0;
+          font-size: 85%;
+          margin: 0;
+          padding: 0.2em 0.4em;
         }
       `}</style>
     </div>
