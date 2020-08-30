603cbc44722ef715ce8cb2cadaa418913fa7ecbd
ScottAgirs <scott@ijs.to>
2020-08-30 16:17:09 -0400

    [optimize] _app.js
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,11 @@
-import App from "next/app";
+import Page from '../components/Page';
 
-import Page from "../components/Page";
+const App = ({ Component, pageProps }) => {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+};
 
-class AppWrapper extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    pageProps.query = ctx.query;
-
-    return { pageProps };
-  }
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-    );
-  }
-}
-
-export default AppWrapper;
+export default App;
