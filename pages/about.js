38d0dd62a381b13179e9c2702074a9b7498a7132
ScottAgirs <scott@ijs.to>
2020-08-30 18:23:33 -0400

    mess up linting for example
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,11 @@
 export const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      
+      
+      
+      
+      <h1        style={{marginTop: 64}}>About Page</h1>
 
       <p>
         Interested in learning more about what is Next.js?{" "}
@@ -14,50 +18,20 @@ export const About = () => {
         </a>
       </p>
       <p>
-        This simple app is intended as a base for any other{" "}
+        A proper ESLint + Prettier setup will save you tonnes of time coding{" "}
         <a
           href="https://ijs.to" 
           target="_blank"
           rel="noopener noreferrer"
         >
-          iJS.to
+          learn more about this setup here.
         </a>{" "}
-        or your own starter projects to eliminate the boilerplate.
-      </p>
-      <h2>Get Started in (3 minutes)</h2>
-      <ul>
-        <li>
-          1. Clone the the{" "}
-          <a href="https://github.com/ijsto/inextjs">starter repo</a>
-          <p>
-            <code>git clone git@github.com:ijsto/inextjs.git</code>
-          </p>
-        </li>
-        <li>
-          2. Enter the directory
-          <p>
-            <code>cd inextjs</code>
-          </p>
-        </li>
-        <li>
-          3. Install project dependencies
-          <p>
-            <code>yarn</code> or <code>npm install</code> or <code>npm i</code>
-          </p>
-        </li>
-        <li>
-          To run the project locally:
-          <p>
-            <code>yarn dev</code> or <code>npm run dev</code>
-          </p>
-        </li>
-        <li>
-          Starter project is live at:{" "}
-          <a href="http://localhost:3000">http://localhost:3000</a>
-        </li>
-      </ul>
 
-      <h2>The starter is now ready - have fun!</h2>
+      </p>
+     
+
+
+      <h2>Happy coding!</h2>
       <style jsx>{`
         * {
           line-height: 1.5;
